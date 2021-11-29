c3bc7c8871083fdac3ebd8063742a24c0d19a62a
kiana <kia.kasir02@gmali.com>
2021-11-29 23:13:39 +0330

    Add protect middleware to access the route if authenticated

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -58,3 +58,34 @@ exports.login = async (req, res, next) => {
     });
   }
 };
+
+exports.protect = async (req, res, next) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+      res.status(403).json({
+        error: "please login first !",
+      });
+      return next();
+    }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    const currentUser = await User.findById(decoded.id);
+
+
+    req.user = currentUser;
+    next();
+
+  } catch (err) {
+    res.status(403).json({
+      error: err,
+    });
+  }
+};
+  
