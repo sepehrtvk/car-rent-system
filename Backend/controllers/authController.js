7f3b941deededc4e5b25b4b9483f2e8fa6d0f675
kiana <kia.kasir02@gmali.com>
2021-11-30 12:10:55 +0330

    Add restrictTo middleware for admin access

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 exports.signup = async (req, res, next) => {
   try {
@@ -10,7 +10,7 @@ exports.signup = async (req, res, next) => {
     });
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       token,
       data: {
         user: newUser,
@@ -30,15 +30,15 @@ exports.login = async (req, res, next) => {
 
     if (!email || !password) {
       res.status(404).json({
-        message: 'incorrect email or password',
+        message: "incorrect email or password",
       });
       return next();
     }
-    const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user || !(await user.correctPassword(password, user.password))) {
       res.status(404).json({
-        message: 'incorrect email or password',
+        message: "incorrect email or password",
       });
       return next();
     }
@@ -48,10 +48,10 @@ exports.login = async (req, res, next) => {
     });
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       token,
     });
-    next()
+    next();
   } catch (err) {
     res.status(401).json({
       error: err,
@@ -64,9 +64,9 @@ exports.protect = async (req, res, next) => {
     let token;
     if (
       req.headers.authorization &&
-      req.headers.authorization.startsWith('bearer')
+      req.headers.authorization.startsWith("bearer")
     ) {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split(" ")[1];
     }
     if (!token) {
       res.status(403).json({
@@ -78,14 +78,24 @@ exports.protect = async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
     const currentUser = await User.findById(decoded.id);
 
-
     req.user = currentUser;
     next();
-
   } catch (err) {
     res.status(403).json({
       error: err,
     });
   }
 };
-  
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (req.user && !roles.includes(req.user.role)) {
+      res.status(403).json({
+        error: "you don not have premission",
+      });
+      return next();
+    }
+    next();
+  };
+};
+ 
