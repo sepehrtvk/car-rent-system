e1d482e6ff627b904467088b737fe86cddaec3d8
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2021-12-01 13:45:18 +0330

    Add database config and user routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 
 
+dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db)
+  .then(() => console.log('succesfull'))
+  .catch((err) => console.log(err));
+
+
+
+
+const userRouter = require('./routes/userRoutes');
 
 
 // MIDDLEWARES
@@ -13,10 +31,31 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST,OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 
 
-const port = process.env.PORT || 3000;
+// ROUTES
+app.use('/api/v1/users', userRouter);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,26 +1,50 @@
-const User = require("../models/userModel");
-const jwt = require("jsonwebtoken");
-const { promisify } = require("util");
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
     });
 
-    res.status(201).json({
-      status: "success",
-      token,
-      data: {
-        user: newUser,
-      },
-    });
+    createSendToken(newUser, 201, res);
   } catch (err) {
-    res.status(401).json({
-      error: err,
-    });
-    return next();
+    console.log(err);
   }
 };
 
@@ -30,30 +54,22 @@ exports.login = async (req, res, next) => {
 
     if (!email || !password) {
       res.status(404).json({
-        message: "incorrect email or password",
+        message: 'incorrect email or password',
       });
       return next();
     }
-    const user = await User.findOne({ email }).select("+password");
+    const user = await User.findOne({ email }).select('+password');
 
     if (!user || !(await user.correctPassword(password, user.password))) {
       res.status(404).json({
-        message: "incorrect email or password",
+        message: 'incorrect email or password',
       });
       return next();
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-
-    res.status(201).json({
-      status: "success",
-      token,
-    });
-    next();
+    createSendToken(user, 200, res);
   } catch (err) {
-    res.status(401).json({
+    res.status(404).json({
       error: err,
     });
   }
@@ -64,13 +80,13 @@ exports.protect = async (req, res, next) => {
     let token;
     if (
       req.headers.authorization &&
-      req.headers.authorization.startsWith("bearer")
+      req.headers.authorization.startsWith('bearer')
     ) {
-      token = req.headers.authorization.split(" ")[1];
+      token = req.headers.authorization.split(' ')[1];
     }
     if (!token) {
       res.status(403).json({
-        error: "please login first !",
+        error: 'please login first !',
       });
       return next();
     }
@@ -86,16 +102,14 @@ exports.protect = async (req, res, next) => {
     });
   }
 };
-
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (req.user && !roles.includes(req.user.role)) {
       res.status(403).json({
-        error: "you don not have premission",
+        error: 'you don not have premission',
       });
       return next();
     }
     next();
   };
 };
- 
