d3004dd8cd6e427262fd08d01a2c3664bf6ef100
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2021-12-07 12:12:25 +0400

    FIx styles

--- Frontend/src/components/Cars/CarModel.js
+++ Frontend/src/components/Cars/CarModel.js
@@ -5,11 +5,11 @@ function CarModel(props) {
     <div className="col-3">
       <div class="card">
         <img src={props.photo} alt={props.name} class="card-img-top"  height="200"/>
-        <div class="card-body">
-          <h5 class="card-title">{props.name}</h5>
-          <p class="card-text">{props.brand}</p>
-          <p class="card-text">{props.color}</p>
-          <a href="#" class="btn btn-primary">
+        <div class="card-body ">
+          <h4 class="card-title fw-bold text-center mb-4 pb-3 border-bottom ">{props.name}</h4>
+          <p class="card-text">برند :‌ {props.brand} </p>
+          <p class="card-text">رنگ : {props.color}</p>
+          <a href="#" class="btn btn-primary text-center mt-4 w-100">
             اجاره خودرو
           </a>
         </div>
